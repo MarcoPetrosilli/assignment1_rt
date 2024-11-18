4a9786f3fdb00cdd689215b94ccc761ac0f47085
Marco <mark99961@gmail.com>
2024-11-18 22:28:43 +0000

    end code, node_2 not tested yet

--- scripts/Distance_node2.py
+++ scripts/Distance_node2.py
@@ -6,10 +6,26 @@ from std_msgs.msg import Float32
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
 
-current_pose1 = None
-current_pose2 = None
+current_pose1 = Pose()
+current_pose2 = Pose()
 
-def callback_turtle(pose,turtle_name):
+current_vel1 = Twist()
+current_vel2 = Twist()
+
+def is_non_zero(twist):
+	return any(v != 0 for v in [twist.linear.x, twist.linear.y, twist.angular.z])
+
+def callback_vel(cmd_vel,turtle_name):
+
+	global current_vel1
+	global current_vel2
+	
+	if turtle_name == 'turtle1':
+		current_vel1 = cmd_vel
+	else:
+		current_vel2 = cmd_vel
+
+def callback_pose(pose,turtle_name):
 
 	global current_pose1
 	global current_pose2
@@ -31,18 +47,25 @@ def checkDistance():
 	    
 	pub_dis = rospy.Publisher('distance', Float32, queue_size=10)
 	    
-	rospy.Subscriber('turtle1/pose', Pose, callback_turtle, callback_args='turtle1')
+	rospy.Subscriber('turtle1/pose', Pose, callback_pose, callback_args='turtle1')
+	rospy.Subscriber('turtle1/cmd_vel', Twist, callback_vel, callback_args='turtle1')
 	pub_vel1 = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+	
 	    
-	rospy.Subscriber('turtle2/pose', Pose, callback_turtle, callback_args='turtle2')
+	rospy.Subscriber('turtle2/pose', Pose, callback_pose, callback_args='turtle2')
+	rospy.Subscriber('turtle2/cmd_vel', Twist, callback_vel, callback_args='turtle2')
 	pub_vel2 = rospy.Publisher('turtle2/cmd_vel', Twist, queue_size=10)
+	
+	
 	    
-	rate = rospy.Rate(10)
+	rate = rospy.Rate(1)
 	
 	distance = Float32()
 	
-	my_vel1 = Twist()
-	my_vel2 = Twist()
+	my_vel = Twist()
+	my_vel.linear.x = 0.0;
+	my_vel.linear.y = 0.0;
+	my_vel.angular.z = 0.0;
     
     
 	while not rospy.is_shutdown():
@@ -50,10 +73,12 @@ def checkDistance():
 		distance = math.sqrt((current_pose1.x-current_pose2.x)**2+(current_pose1.y-current_pose2.y)**2)
 		
 		if distance < treshold:
-			
-		else:
+			if is_non_zero(current_vel1):
+				pub_vel1.publish(my_vel)
+				
+			elif is_non_zero(current_vel2):
+				pub_vel1.publish(my_vel)
 		
-		pub.publish(hello_str)
 		rate.sleep()
 
 if __name__ == '__main__':
