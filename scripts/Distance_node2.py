1614b159c9f54faad898393ff1ec341cc4eb72ec
Marco <mark99961@gmail.com>
2024-11-21 17:17:18 +0000

    scalar product control and limits check have been implemented

--- scripts/Distance_node2.py
+++ scripts/Distance_node2.py
@@ -12,24 +12,54 @@ current_pose2 = Pose()
 current_vel1 = Twist()
 current_vel2 = Twist()
 
-blocked1 = false
-blocked2 = false
+blocked1 = False
+blocked2 = False
+
+
+
+
+def inverse_velocity(current_velocity):
+	
+	my_vel = Twist()
+	my_vel.linear.x = -current_velocity.linear.x
+	my_vel.linear.y = -current_velocity.linear.y
+	my_vel.angular.z = -current_velocity.angular.z
+	
+	return my_vel
+	
 
 def not_move_to_other_direction(current_pose1, current_pose2, velocity):
-	dx = current_pose1.x - current_pose2.x
-	dy = current_pose1.y - current_pose2.y
+
+	dx = current_pose2.x - current_pose1.x
+	dy = current_pose2.y - current_pose1.y
 	
 	scalar_product = dx * velocity.linear.x + dy * velocity.linear.y
+	norm_dist = calculate_module(dx, dy)
+	norm_vel = calculate_module(velocity.linear.x, velocity.linear.y)
 	
-	return scalar_product>0
+	#print("sc: " + str(scalar_product))
+	#print("norm* : " + str(scalar_product))
+	
+	cos_angle = scalar_product/(norm_dist*norm_vel)
+	angle = math.acos(cos_angle)
+	
+	return angle < math.radians(45)
+	
+	
+def calculate_module(x, y):
+
+	return math.sqrt((x)**2+(y)**2)
 
 
 def is_out_of_limits(pose, low_limit, high_limit):
+
     return pose.x < low_limit or pose.x > high_limit or pose.y < low_limit or pose.y > high_limit
 
 
-def is_non_zero(twist):
+def is_not_zero(twist):
+
 	return any(v != 0 for v in [twist.linear.x, twist.linear.y, twist.angular.z])
+	
 
 def callback_vel(cmd_vel,turtle_name):
 
@@ -40,6 +70,7 @@ def callback_vel(cmd_vel,turtle_name):
 		current_vel1 = cmd_vel
 	elif turtle_name == 'turtle2':
 		current_vel2 = cmd_vel
+		
 
 def callback_pose(pose,turtle_name):
 
@@ -52,6 +83,7 @@ def callback_pose(pose,turtle_name):
 		current_pose2 = pose
 	
 	#rospy.loginfo("The position of %s is: %f, %f, %f",turtle_name ,pose.x, pose.y, pose.theta);
+	
 
 def checkDistance(): 
 
@@ -78,6 +110,7 @@ def checkDistance():
 	
 	distance = Float32()
 	
+	
 	my_vel = Twist()
 	my_vel.linear.x = 0.0;
 	my_vel.linear.y = 0.0;
@@ -85,22 +118,46 @@ def checkDistance():
     
     
 	while not rospy.is_shutdown():
-	
-		distance = math.sqrt((current_pose1.x-current_pose2.x)**2+(current_pose1.y-current_pose2.y)**2)
+
+		dist_x = current_pose1.x-current_pose2.x
+		dist_y = current_pose1.y-current_pose2.y
+		distance = calculate_module(dist_x, dist_y)
 		
-		if distance < treshold or is_out_of_limits(current_pose1, low_lim, high_lim) or is_out_of_limits(current_pose2, low_lim, high_lim):
 		
-			if is_non_zero(current_vel1) and not_move_to_other_direction(current_pose1, current_pose2, current_vel1):
+		close_turtles = distance < treshold
+		t1_out_limits = is_out_of_limits(current_pose1, low_lim, high_lim)
+		t2_out_limits = is_out_of_limits(current_pose2, low_lim, high_lim)
+		
+		if close_turtles:
+		
+			if is_not_zero(current_vel1) and not_move_to_other_direction(current_pose1, current_pose2, current_vel1):
 			
 				pub_vel1.publish(my_vel)
 				print("turtle1 stopped, and the distance is %f",distance)
 				
-			elif is_non_zero(current_vel2) and not_move_to_other_direction(current_pose1, current_pose2, current_vel2):
+			elif is_not_zero(current_vel2) and not_move_to_other_direction(current_pose1, current_pose2, current_vel1):
 			
 				pub_vel2.publish(my_vel)
 				print("turtle2 stopped and the distance is %f",distance)
+				
+				
+		if t1_out_limits or t2_out_limits:
+				
+			if t1_out_limits:
+			
+				my_vel = inverse_velocity(current_vel1)
+				pub_vel1.publish(my_vel)
+				
+			elif t2_out_limits:
+			
+				my_vel = inverse_velocity(current_vel2)
+				pub_vel2.publish(my_vel)
+				
+			
 		
 		rate.sleep()
+		
+		
 
 if __name__ == '__main__':
 
